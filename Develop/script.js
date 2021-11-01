a1cc76e9c9305b06f48a406bc6459a3796b4a53a
Erica <ericamisiorski@gmail.com>
2021-10-31 22:22:23 -0400

    edit

---
+++ Develop/script.js
@@ -0,0 +1,47 @@
+$(document).ready(function () {
+
+    $(".saveBtn").on('click', function() {
+      var listItem = $(".description").val();
+      var time = $(".time-block").val();
+        window.localStorage.setItem(listItem, time);
+    })
+
+
+
+    function timeTracker() {
+        //Display today's time and date using moment
+        var currentTime = moment().format('H');
+        console.log(currentTime);
+        //get number from time block id 
+        $(".time-block").each(function () {
+            var scheduleTime = parseInt($(this).attr("id"));
+            console.log(scheduleTime);
+
+        //compare the schedule number to the time and connect to colors
+        //if the current time is greater, boxes are gray
+            if (scheduleTime < currentTime) {
+                $(this).removeClass("future");
+                $(this).removeClass("present");
+                $(this).addClass("past");
+            }
+        //if the current time equals the schedule time, box is red
+                else if (scheduleTime === currentTime) {
+                    $(this).removeClass("future");
+                    $(this).addClass("present");
+                    $(this).removeClass("past");
+                }
+        //if the current time is before the scheduled time, boxes are green
+                else if (scheduleTime > currentTime) {
+                    $(this).addClass("future");
+                    $(this).removeClass("present");
+                    $(this).removeClass("past");
+                }
+            }
+
+        )
+       
+    } 
+    timeTracker(); 
+})
+
+
